eeedf9cb812497848292ab2789e682de75218bec
JaeHun <ja-value@naver.com>
2021-01-27 10:54:13 +0900

    설정 연습

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+// TODO : TodoList 구현
+// FIXME
+function TodoList() {
+  const title = "...";
+  const name = "...";
+}
 
 function App() {
   return (
